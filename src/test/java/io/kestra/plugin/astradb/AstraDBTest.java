b3d0fe30eee1075ea52f43afdeb1c1e0f1e79040
Mayur Choubey <mayurchoubey123@gmail.com>
2023-05-05 18:03:00 +0530

    test

--- src/test/java/io/kestra/plugin/astradb/AstraDBTest.java
+++ src/test/java/io/kestra/plugin/astradb/AstraDBTest.java
@@ -1,23 +1,21 @@
-package io.kestra.plugin.templates;
+package io.kestra.plugin.astradb;
 
 import com.google.common.collect.ImmutableMap;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 
 import jakarta.inject.Inject;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 /**
  * This test will only test the main task, this allow you to send any input
  * parameters to your task and test the returning behaviour easily.
  */
 @MicronautTest
-class ExampleTest {
+@Disabled("no server for unit test")
+class AstraDBTest {
     @Inject
     private RunContextFactory runContextFactory;
 
@@ -25,12 +23,5 @@ class ExampleTest {
     void run() throws Exception {
         RunContext runContext = runContextFactory.of(ImmutableMap.of("variable", "John Doe"));
 
-        Example task = Example.builder()
-            .format("Hello {{ variable }}")
-            .build();
-
-        Example.Output runOutput = task.run(runContext);
-
-        assertThat(runOutput.getChild().getValue(), is(StringUtils.reverse("Hello John Doe")));
     }
 }
